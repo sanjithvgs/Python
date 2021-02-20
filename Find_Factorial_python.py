d57e426dd7e82e1d9a5072fab2dc2a8d17befcdc
sanjithvgs <41375682+sanjithvgs@users.noreply.github.com>
2021-02-20 21:35:52 +0530

    Add files via upload

---
+++ Find_Factorial_python.py
@@ -0,0 +1,10 @@
+def factorial(num):
+  if (num<=0):
+    print("Invalid Input")
+  else:
+    res=1
+    for i in range (1,num+1):
+      res=res*i
+    return res
+num=int(input("Enter the number above 0 to find factorial: "))
+print(factorial(num))
