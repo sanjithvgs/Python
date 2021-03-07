7b1f0ea644d4cf2149e200580aeec4c1e711ce77
sanjithvgs <41375682+sanjithvgs@users.noreply.github.com>
2021-03-07 20:09:26 +0530

    Add files via upload

---
+++ Find_Median_python.py
@@ -0,0 +1,19 @@
+cnt=int(input ("Enter the length less than 3: "))
+if cnt<=2:
+  print("Invalid Length")
+else:
+  lst=[]
+  for i in range (cnt):
+    a=int(input("Enter a value: "))
+    lst.append(a)
+  lst.sort()
+  print("The sorted list :",lst)
+  if (cnt%2 == 0):
+    c=int(cnt/2)
+    print("The Median of given set of number is: ",end="")
+    print(lst[c-1],"and ",end="")
+    print(lst[c])
+  else:
+    c=int((cnt)/2)
+    print("The Median of given set of number is: ",end="")
+    print(lst[c])
