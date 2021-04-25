846b8caa22de5de60b31a70f0bc823ccd9c22554
sanjithvgs <41375682+sanjithvgs@users.noreply.github.com>
2021-04-25 22:42:05 +0530

    Update List_Comprehensions_python.py

--- List_Comprehensions_python.py
+++ List_Comprehensions_python.py
@@ -1,6 +1,5 @@
 ''' Given three integers x,y and z representing the dimensions of a cuboid along with an integer n. 
-    Print a list of all possible coordinates given by (x,y,z) on a 3D grid 
-    where the sum of i+j+k is not equal to n'''
+    Print a list of all possible coordinates given by (x,y,z) on a 3D where the sum of i+j+k is not equal to n'''
 
 x = int(input())
 y = int(input())
