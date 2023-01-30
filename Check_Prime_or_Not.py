743df040008322142c20e146a0e05ad2c49eabd4
sanjithvgs <41375682+sanjithvgs@users.noreply.github.com>
2023-01-30 23:30:10 +0530

    Small update

--- Check_Prime_or_Not.py
+++ Check_Prime_or_Not.py
@@ -1,7 +1,7 @@
 num=int(input("Enter the number to check prime or not: "))
-res="PRIME"
+res="PRIME NUMBER"
 for i in range(2,num):
   if num%i==0:
-    res="NON PRIME"
+    res="NON PRIME NUMBER"
     break
 print(res)
