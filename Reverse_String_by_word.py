523567a61959c6107381b38b3941fc292d3d3ff6
sanjithvgs <41375682+sanjithvgs@users.noreply.github.com>
2023-07-09 22:12:00 +0530

    Add files via upload

---
+++ Reverse_String_by_word.py
@@ -0,0 +1,9 @@
+A=input("Enter the string: ")
+word=A.split()
+reverse_string=" ".join(reversed(word))
+print("The reverse string is: ",reverse_string)
+
+
+# output:
+# Enter the string: This is python world
+# The reverse string is:  world python is This
